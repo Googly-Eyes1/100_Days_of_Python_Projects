5257159808898b6e90b001d1fe67cfe26189cf78
Abirami Saravanan <113621331+Googly-Eyes1@users.noreply.github.com>
2024-10-15 13:50:10 +0530

    Create Day1.py

---
+++ Day 1: Band Name Generator/Day1.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator!")
+city_name = input("Which city did you grow up in ?\n")
+pet_name = input("Enter the name of your pet:\n")
+print("Your band name could be" + " "+ city_name +" "+ pet_name)
